7bb7fcdfc978c8253c3bb757e90aa3f633345641
Jarosław Terejko <j.terejko@gmail.com>
2019-09-11 18:57:03 +0200

    created project with create-react-app added homepage

---
+++ src/Components/Homepage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Homepage.scss'
+
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h2 className="title">Hats</h2>
+            <span className="subtitle">Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
