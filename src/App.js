ca088b5ee3ddd2afd8a5424c3d4e2f3b6e68212b
Jarosław Terejko <j.terejko@gmail.com>
2019-09-12 08:46:57 +0200

    setup Switch Route withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Homepage from './Pages/Homepage/Homepage'
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
+
+
+const HatsPage = () => {
+  return (
+   <div> hats page</div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <Homepage />
+    <Switch>
+    <Route exact path="/" component={Homepage} />
+    <Route path="/hats" component={HatsPage} />
+
+    </Switch>
     </div>
   );
 }
